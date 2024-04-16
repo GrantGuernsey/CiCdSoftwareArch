61e3ea598f80214b14bd10f8e9eb4aad7fad2c84
GrantGuernsey <78874644+GrantGuernsey@users.noreply.github.com>
2024-04-15 22:08:58 -0400

    Updated class names in unit test library

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -13,36 +13,36 @@ namespace UnitTest1
 		TEST_METHOD(TestMethod1)
 		{
 
-			CoolClass coolClass;
-			Assert::AreEqual(2, coolClass.Calc(1, 2));
+			CInClass CInClass;
+			Assert::AreEqual(2, CInClass.Calc(1, 2));
 		}
 
 		TEST_METHOD(TestMethod2)
 		{
 
-			CoolClass coolClass;
-			Assert::AreEqual(5, coolClass.Calc(3, 2));
+			CInClass CInClass;
+			Assert::AreEqual(5, CInClass.Calc(3, 2));
 		}
 
 		TEST_METHOD(TestMethod3)
 		{
 
-			CoolClass coolClass;
-			Assert::IsTrue(coolClass.Calc(3, 2) > 0);
+			CInClass CInClass;
+			Assert::IsTrue(CInClass.Calc(3, 2) > 0);
 		}
 
 		TEST_METHOD(TestMethod4)
 		{
 
-			CoolClass coolClass;
-			Assert::AreEqual(5, coolClass.Calc(3, 2));
+			CInClass CInClass;
+			Assert::AreEqual(5, CInClass.Calc(3, 2));
 		}
 
 		TEST_METHOD(TestMethod5)
 		{
 
-			CoolClass coolClass;
-			Assert::AreEqual(5, coolClass.Ming(3, 2));
+			CInClass CInClass;
+			Assert::AreEqual(5, CInClass.Ming(3, 2));
 		}
 	};
 }
